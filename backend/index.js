ab78f2132479e3ef01f7f52f1eac1df8d3dcb5dc
Tylwyz <tylwyz1@gmail.com>
2024-04-11 20:20:44 -0500

    Added modules and slight changes to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import mysql from "mysql"
+import mysql from 'mysql'
+import cors from 'cors'
 
 const app = express()
 
@@ -11,9 +12,11 @@ const db = mysql.createConnection({
 
 })
 
-app.use(express.json)
+app.use(express.json())
+app.use(cors("*"))
 
 app.get("/", (req,res)=>{
+    console.log("Entered")
     res.json("This is the backend")
 })
 
